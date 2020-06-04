565645f6f762656777315053f42ddcc662d28401
Leisn <cando.i@outlook.com>
2020-06-04 12:49:44 +0800

    fix upload issue
    There is a difference between upload single file and multiple files , because of 'ctx.request.files' type. When upload a single file type is 'object', and when upload multiple files type is 'Array'.

--- apps/www/index.js
+++ apps/www/index.js
@@ -5,23 +5,31 @@ const path = require('path');
 const fs = require('fs');
 
 router.get('/', async ctx => {
-  await ctx.render('index',{
-    title:'Hello'
+  await ctx.render('index', {
+    title: 'HELLO'
   })
 });
 
 const tmp_dir = os.tmpdir();
 const upload_dir = path.join(__dirname, "../../uploads/");
 
+function saveFile(file) {
+  const reader = fs.createReadStream(file.path);
+  const stream = fs.createWriteStream(path.join(upload_dir, Math.random().toString()));
+  // const stream = fs.createWriteStream(path.join(tmp_dir, Math.random().toString()));
+  reader.pipe(stream);
+  console.log('uploading %s -> %s', file.name, stream.path);
+}
+
 router.post('/upload', async ctx => {
+  ctx.throw(new Error('boom boom'));
   const files = ctx.request.files.file;
-  for (const key in files) {
-    const file = files[key];
-    const reader = fs.createReadStream(file.path);
-    const stream = fs.createWriteStream(path.join(upload_dir, Math.random().toString()));
-    // const stream = fs.createWriteStream(path.join(tmp_dir, Math.random().toString()));
-    reader.pipe(stream);
-    console.log('uploading %s -> %s', file.name, stream.path);
+  if (files instanceof Array) {
+    for (const key in files) {
+      saveFile(files[key])
+    }
+  } else {
+    saveFile(files);
   }
   ctx.body = 'ok';
 });
