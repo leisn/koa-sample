393635ab3462c1efa828a00e955c9a2dc2d75294
Leisn <cando.i@outlook.com>
2020-06-04 11:29:15 +0800

    add upload file handler

--- apps/www/index.js
+++ apps/www/index.js
@@ -1,10 +1,29 @@
 const Router = require('koa-router');
 const router = new Router();
+const os = require('os');
+const path = require('path');
+const fs = require('fs');
 
 router.get('/', async ctx => {
   await ctx.render('index',{
-    title:'WWW'
+    title:'Hello'
   })
+});
+
+const tmp_dir = os.tmpdir();
+const upload_dir = path.join(__dirname, "../../uploads/");
+
+router.post('/upload', async ctx => {
+  const files = ctx.request.files.file;
+  for (const key in files) {
+    const file = files[key];
+    const reader = fs.createReadStream(file.path);
+    const stream = fs.createWriteStream(path.join(upload_dir, Math.random().toString()));
+    // const stream = fs.createWriteStream(path.join(tmp_dir, Math.random().toString()));
+    reader.pipe(stream);
+    console.log('uploading %s -> %s', file.name, stream.path);
+  }
+  ctx.body = 'ok';
 });
 
 module.exports = router;
